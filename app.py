334eeb5fab9ee427017948d802cdb8af6a5e8fb3
malik <malikmatiullah@outlook.com>
2023-05-15 22:57:32 +0500

    Initial Fastapi App

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-def hello_world():
-    return 'Hello Sammy!'
+
+@app.get("/", response_class=HTMLResponse)
+async def hello_world(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
